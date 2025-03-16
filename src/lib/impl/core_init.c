0fab828ac4e14d2d40800018fdfc298e63654fd5
Steven Hangger <longhronshen@hotmail.com>
2025-03-16 21:47:17 +0800

    fix(lib): add ex version of extension init entrypoint

--- src/lib/impl/core_init.c
+++ src/lib/impl/core_init.c
@@ -4,3 +4,8 @@
 int core_init(const char *dummy) {
   return sqlite3_auto_extension((void *)sqlite3_vec_init);
 }
+
+int sqlite3_vecex_init(sqlite3 *db, char **pzErrMsg,
+                       const sqlite3_api_routines *pApi) {
+  return sqlite3_vec_init(db, pzErrMsg, pApi);
+}
